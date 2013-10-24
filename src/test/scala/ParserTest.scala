1ca149e967ee1380b8383cc574e3b56a36529156
Josh <joshamosricher@gmail.com>
2013-10-24 16:25:57 -0700

    Finished parser, found a logic problem with my compiler idea, may have to simplify the language to fit into lucenes query mechanisms

---
+++ src/test/scala/ParserTest.scala
@@ -0,0 +1,56 @@
+package com.protomapper.test
+import org.scalatest.FunSuite
+import java.io.File
+import com.protomapper.compile._
+import org.biojava3.core.sequence.ProteinSequence
+
+class PatternParserTest extends FunSuite {
+  test("Test PatternParser::parse(TEST)") {
+    expect("Term(List(Str(T), Str(E), Str(S), Str(T)))") {
+      val parse = new PatternParser
+      parse.parse("TEST").toString()
+    }
+  }
+  test("""Test PatternParser::parse(T(ES)T)""") {
+    expect("Term(List(Str(T), Term(List(Str(E), Str(S))), Str(S), Str(T)))") {
+      val parse = new PatternParser
+      parse.parse("T(ES)ST").toString()
+    }
+  }
+  test("""Test PatternParser::parse(T[ES]T)""") {
+    expect("Term(List(Str(T), Range(false,List(SubRangeChar(S), SubRangeChar(E))), Str(S), Str(T)))") {
+      val parse = new PatternParser
+      parse.parse("T[ES]ST").toString()
+    }
+  }
+  test("""Test PatternParser::parse(T[^ES]ST)""") {
+    expect("Term(List(Str(T), Range(true,List(SubRangeChar(S), SubRangeChar(E))), Str(S), Str(T)))") {
+      val parse = new PatternParser
+      parse.parse("T[^ES]ST").toString()
+    }
+  }
+  test("""Test PatternParser::parse(T{2,4}EST)""") {
+    expect("Term(List(LenRange(2,4,Str(T)), Str(E), Str(S), Str(T)))") {
+      val parse = new PatternParser
+      parse.parse("T{2,4}EST").toString()
+    }
+  }
+  test("""Test PatternParser::parse(T[A-Z]EST)""") {
+    expect("Term(List(Str(T), Range(false,List(SubRangeMulti(A,Z))), Str(E), Str(S), Str(T)))") {
+      val parse = new PatternParser
+      parse.parse("T[A-Z]EST").toString()
+    }
+  }
+  test("""Test PatternParser::parse(T.EST)""") {
+    expect("Term(List(Str(T), Wild(), Str(E), Str(S), Str(T)))") {
+      val parse = new PatternParser
+      parse.parse("T.EST").toString()
+    }
+  }
+  test("Test PatternParser::parse(HEL[LO](WO)RLD|!!)") {
+    expect("Choice(Term(List(Str(H), Str(E), Str(L), Range(false,List(SubRangeChar(O), SubRangeChar(L))), Term(List(Str(W), Str(O))), Str(R), Str(L), Str(D))),Term(List(Str(!), Str(!))))") {
+      val parse = new PatternParser
+      parse.parse("HEL[LO](WO)RLD|!!").toString()
+    }
+  }
+}
