28165756cfc6b988a5e3dfbb26a5f55c9a8de711
Josh <joshamosricher@gmail.com>
2013-10-27 12:46:50 -0700

    Progress on the compiler, not done yet.

--- src/test/scala/ParserTest.scala
+++ src/test/scala/ParserTest.scala
@@ -4,6 +4,16 @@ import java.io.File
 import com.protomapper.compile._
 import org.biojava3.core.sequence.ProteinSequence
 
+class PatternCompilerTest extends FunSuite {
+  test("Test PatternCompiler") {
+    expect(true){
+    	val parser = new PatternParser
+    	val compile = new PatternCompiler(parser)
+    	compile.compile("ST{2,4}X|XXYZ")
+    }
+  }
+}
+
 class PatternParserTest extends FunSuite {
   test("Test PatternParser::parse(TEST)") {
     expect("Term(List(Str(T), Str(E), Str(S), Str(T)))") {
@@ -11,7 +21,7 @@ class PatternParserTest extends FunSuite {
       parse.parse("TEST").toString()
     }
   }
-  test("""Test PatternParser::parse(T(ES)T)""") {
+  ignore("""Test PatternParser::parse(T(ES)T)""") {
     expect("Term(List(Str(T), Term(List(Str(E), Str(S))), Str(S), Str(T)))") {
       val parse = new PatternParser
       parse.parse("T(ES)ST").toString()
@@ -35,6 +45,12 @@ class PatternParserTest extends FunSuite {
       parse.parse("T{2,4}EST").toString()
     }
   }
+  test("""Test PatternParser::parse(T|E|ST)""") {
+    expect("Term(List(LenRange(2,4,Str(T)), Str(E), Str(S), Str(T)))") {
+      val parse = new PatternParser
+      parse.parse("T|E|ST").toString()
+    }
+  }
   test("""Test PatternParser::parse(T[A-Z]EST)""") {
     expect("Term(List(Str(T), Range(false,List(SubRangeMulti(A,Z))), Str(E), Str(S), Str(T)))") {
       val parse = new PatternParser
@@ -47,7 +63,7 @@ class PatternParserTest extends FunSuite {
       parse.parse("T.EST").toString()
     }
   }
-  test("Test PatternParser::parse(HEL[LO](WO)RLD|!!)") {
+  ignore("Test PatternParser::parse(HEL[LO](WO)RLD|!!)") {
     expect("Choice(Term(List(Str(H), Str(E), Str(L), Range(false,List(SubRangeChar(O), SubRangeChar(L))), Term(List(Str(W), Str(O))), Str(R), Str(L), Str(D))),Term(List(Str(!), Str(!))))") {
       val parse = new PatternParser
       parse.parse("HEL[LO](WO)RLD|!!").toString()
